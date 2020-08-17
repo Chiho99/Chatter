d4990193ad45140b3f899a8f81c60371efde2fc1
Chiho99 <chihomikami.81@gmail.com>
2020-08-17 17:41:29 +0900

    modify-dbconnect

--- dbconnect.php
+++ dbconnect.php
@@ -1,14 +1,17 @@
 <?php
-    $dsn = 'mysql:dbname=Learn_SNS;host=localhost';
-    $user = 'root';
-    $password='';
-    $dbh = new PDO($dsn, $user, $password);
+    // $dsn = 'mysql:dbname=Learn_SNS;host=localhost';
+    // $user = 'root';
+    // $password='';
+    // $dbh = new PDO($dsn, $user, $password);
 
-    // $host = getenv('host'); //MySQLがインストールされてるコンピュータ
-    // $dbname = getenv('dbname'); //使用するDB
-    // $charset = "utf8"; //文字コード
-    // $user = getenv('username'); //MySQLにログインするユーザー名
-    // $password = getenv('password'); //ユーザーのパスワード
+    
+    $host = getenv('host'); //MySQLがインストールされてるコンピュータ
+    $dbname = getenv('dbname'); //使用するDB
+    $charset = "utf8"; //文字コード
+    $user = getenv('username'); //MySQLにログインするユーザー名
+    $password = getenv('password'); //ユーザーのパスワード
+    $dsn = 'mysql:host=$host;dbname=$dbname;charset=$charset';
+    $dbh = new PDO($dsn, $user, $password);
     // SQL文にエラーがあった際、画面にエラーを出力する設定
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $dbh->query('SET NAMES utf8');
