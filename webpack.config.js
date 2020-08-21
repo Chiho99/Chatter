9218823bfd0ad11794d5b35e2c3695c4d453aae0
Chiho99 <chihomikami.81@gmail.com>
2020-08-22 01:02:24 +0900

    modify

--- webpack.config.js
+++ webpack.config.js
@@ -17,5 +17,65 @@ module.exports = {
                  }
                }
         ]
+    },
+
+    entry: __dirname + "assets/js/jquery-migrate-1.4.1.js", //ビルドするファイル
+    output: {
+      path: __dirname +'assets/js', //ビルドしたファイルを吐き出す場所
+      filename: 'bundle.js' //ビルドした後のファイル名
+    },
+      module: {
+      loaders: [
+              //loader
+              {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query://loaderに渡したいクエリパラメータを指定します
+                 {
+                   presets: ['es2015','stage-0']
+                 }
+               }
+        ]
+    },
+
+    entry: __dirname + "assets/js/bootstrap.js", //ビルドするファイル
+    output: {
+      path: __dirname +'assets/js', //ビルドしたファイルを吐き出す場所
+      filename: 'bundle.js' //ビルドした後のファイル名
+    },
+      module: {
+      loaders: [
+              //loader
+              {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query://loaderに渡したいクエリパラメータを指定します
+                 {
+                   presets: ['es2015','stage-0']
+                 }
+               }
+        ]
+    },
+
+    entry: __dirname + "assets/js/app.js", //ビルドするファイル
+    output: {
+      path: __dirname +'assets/js', //ビルドしたファイルを吐き出す場所
+      filename: 'bundle.js' //ビルドした後のファイル名
+    },
+      module: {
+      loaders: [
+              //loader
+              {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query://loaderに渡したいクエリパラメータを指定します
+                 {
+                   presets: ['es2015','stage-0']
+                 }
+               }
+        ]
     }
   };
