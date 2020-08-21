0a9c7d5d66e3b6ba4013cbfadeb658505f7f7110
Chiho99 <chihomikami.81@gmail.com>
2020-08-22 00:54:27 +0900

    add webpack

--- layouts/footer.php
+++ layouts/footer.php
@@ -1,4 +1,5 @@
-<script src="assets/js/jquery-3.1.1.js"></script>
-<script src="assets/js/jquery-migrate-1.4.1.js"></script>
-<script src="assets/js/bootstrap.js"></script>
-<script src="assets/js/app.js"></script>
+<script type="text/javascript" src="assets/js/jquery-3.1.1.js"></script>
+<script type="text/javascript"src="assets/js/jquery-migrate-1.4.1.js"></script>
+<script type="text/javascript" src="assets/js/bootstrap.js"></script>
+<script type="text/javascript" src="assets/js/app.js"></script>
+<script type="text/javascript" src="assets/js/bundle.js"></script>
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    entry: __dirname + "assets/js/jquery-3.1.1.js", //ビルドするファイル
+    output: {
+      path: __dirname +'assets/js', //ビルドしたファイルを吐き出す場所
+      filename: 'bundle.js' //ビルドした後のファイル名
+    },
+      module: {
+      loaders: [
+              //loader
+              {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query://loaderに渡したいクエリパラメータを指定します
+                 {
+                   presets: ['es2015','stage-0']
+                 }
+               }
+        ]
+    }
+  };
