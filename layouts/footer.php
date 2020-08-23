67f293daa6ccb808971b972fc6a1db1990a144f9
Chiho99 <chihomikami.81@gmail.com>
2020-08-23 23:17:31 +0900

    modify

--- layouts/footer.php
+++ layouts/footer.php
@@ -1,3 +1,4 @@
+<script defer src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 <script src="assets/js/jquery-3.1.1.js"></script>
 <script src="assets/js/jquery-migrate-1.4.1.js"></script>
 <script src="assets/js/bootstrap.js"></script>
--- layouts/header.php
+++ layouts/header.php
@@ -3,7 +3,6 @@
 <head>
     <meta charset="utf-8">
     <title>Chatter</title>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
     <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
     <link rel="stylesheet" type="text/css" href="assets/font-awesome/css/font-awesome.css">
     <link rel="stylesheet" type="text/css" href="assets/css/style.css">
